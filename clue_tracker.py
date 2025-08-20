7be62f64d01c654b8933d2394ec6688f6248c695
Ellie <user@users-MacBook-Air.local>
2025-08-20 15:21:38 +0200

    clue character tracker with nested dictionary and display function

---
+++ clue_tracker.py
@@ -0,0 +1,23 @@
+clue = {}
+
+
+def pretty_print():
+    print()
+
+    for key, value in clue.items():
+        # moves along every 'key:subDictionary' pair and outputs the key (the name of the character).
+        print(key, end=": ")
+        for subKey, subValue in value.items():
+            # (nested) `for` loop moves along every subkey and subvalue in each subDictionary.
+            print(subKey, subValue, end=" | ")
+        print()
+
+
+while True:
+    name = input("Name: ")
+    location = input("Location: ")
+    weapon = input("Weapon: ")
+
+    clue[name] = {"location": location, "weapon": weapon}
+
+    pretty_print()
